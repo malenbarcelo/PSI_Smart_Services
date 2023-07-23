7628693f221bc6fa1244e54e8703f8073dae39cc
malenbarcelo <barcelomalen@gmail.com>
2023-07-23 16:53:05 -0300

    23072023

---
+++ public/js/header.js
@@ -0,0 +1,11 @@
+import { dominio } from "./dominio.js"
+
+window.addEventListener('load',async()=>{
+
+    const menuMobile = document.getElementById('menuMobile')
+    const burgerMenu = document.getElementById('burgerMenu')
+
+    burgerMenu.addEventListener("click",async(e)=>{
+        menuMobile.classList.toggle('notVisible')
+    })
+})
